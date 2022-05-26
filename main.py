68c811c4e45ffe286ccc0fd38e101d1af8ac48f7
Hmmlopl <75092363+hmmlaple@users.noreply.github.com>
2022-05-26 21:42:25 +0100

    lmao

---
+++ main.py
@@ -0,0 +1,11 @@
+import urllib.request
+url = 'https://camo.githubusercontent.com/49c451a74bea3abaa65044deaf157948fb77e1a0fcf9471e49a15e99abc0f1b0/68747470733a2f2f6b6f6d617265762e636f6d2f67687076632f3f757365726e616d653d686d6d6c61706c65266c6162656c3d50726f66696c65253230766965777326636f6c6f723d306537356236267374796c653d666c6174'
+def main():
+    while True:
+        urllib.request.urlretrieve(url)
+        urllib.request.urlretrieve(url)
+        urllib.request.urlretrieve(url)
+        urllib.request.urlretrieve(url)
+        urllib.request.urlretrieve(url)
+        urllib.request.urlretrieve(url)
+main()
