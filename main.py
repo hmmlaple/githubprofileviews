031f3d2cee5a9472d5190ec218bb941781226ea9
Hmmlopl <75092363+hmmlaple@users.noreply.github.com>
2022-05-26 21:47:13 +0100

    remove typo

--- main.py
+++ main.py
@@ -3,9 +3,4 @@ url = 'put ur profile conter here'
 def main():
     while True:
         urllib.request.urlretrieve(url)
-        urllib.request.urlretrieve(url)
-        urllib.request.urlretrieve(url)
-        urllib.request.urlretrieve(url)
-        urllib.request.urlretrieve(url)
-        urllib.request.urlretrieve(url)
 main()
